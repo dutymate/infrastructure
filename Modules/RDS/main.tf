05907d7e00c5908c7eb8566921698a8cbad77b1d
Jongwoo Han <jongwooo.han@gmail.com>
2025-07-31 10:45:46 +0900

    chore(rds): Upgrade MySQL engine version to 8.4.5

--- Modules/RDS/main.tf
+++ Modules/RDS/main.tf
@@ -8,30 +8,31 @@ resource "aws_db_subnet_group" "dbsg" {
 }
 
 resource "aws_db_instance" "db" {
-  allocated_storage       = 20
-  engine                  = "mysql"
-  engine_version          = "8.0.40"
-  instance_class          = "db.t3.micro"
-  username                = var.mysql_username
-  password                = var.mysql_password
-  identifier              = "dutymate-db"
-  multi_az                = false
-  db_subnet_group_name    = aws_db_subnet_group.dbsg.name
-  vpc_security_group_ids  = [var.sg_mysql_id]
-  parameter_group_name    = aws_db_parameter_group.db_params.name
-  kms_key_id              = var.kms_rds_key_arn
-  storage_encrypted       = true
-  skip_final_snapshot     = true
-  backup_retention_period = 7
+  allocated_storage           = 20
+  engine                      = "mysql"
+  engine_version              = "8.4.5"
+  instance_class              = "db.t3.micro"
+  username                    = var.mysql_username
+  password                    = var.mysql_password
+  identifier                  = "dutymate-db"
+  multi_az                    = false
+  db_subnet_group_name        = aws_db_subnet_group.dbsg.name
+  vpc_security_group_ids      = [var.sg_mysql_id]
+  parameter_group_name        = aws_db_parameter_group.db_parameter_group.name
+  kms_key_id                  = var.kms_rds_key_arn
+  storage_encrypted           = true
+  skip_final_snapshot         = true
+  backup_retention_period     = 7
+  allow_major_version_upgrade = false
 
   tags = {
     Name = "dutymate-db"
   }
 }
 
-resource "aws_db_parameter_group" "db_params" {
-  name   = "dutymate-db-params"
-  family = "mysql8.0"
+resource "aws_db_parameter_group" "db_parameter_group" {
+  name   = "dutymate-db-parameter-group"
+  family = "mysql8.4"
 
   parameter {
     name  = "time_zone"
@@ -39,6 +40,6 @@ resource "aws_db_parameter_group" "db_params" {
   }
 
   tags = {
-    Name = "dutymate-db-params"
+    Name = "dutymate-db-parameter-group"
   }
 }
